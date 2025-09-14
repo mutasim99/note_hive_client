258f9f3e739cc98fb30c1d8c58ef6dbf2299e81c
mutasim99 <virginmr882@gmail.com>
2025-09-14 18:53:30 +0600

    added some styles

--- src/components/Dashboard/MyProfile.jsx
+++ src/components/Dashboard/MyProfile.jsx
@@ -251,8 +251,8 @@ const MyProfile = () => {
                 </Card>
             </div>
             {/* Show all subjects */}
-            <div>
-                <h2>My Materials</h2>
+            <div className='m-10'>
+                <h2 className='text-2xl font-bold text-white mb-6 text-center drop-shadow-lg'>My Materials</h2>
                 <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                     {
                         subjects.map((sub, idx) => <UserSubjectsCard
